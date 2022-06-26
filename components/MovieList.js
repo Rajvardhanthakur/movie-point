49924ee7682108231f7a56620fa047eaedfb150d
Rajvardhanthakur <rajthakur3619@gmail.com>
2022-06-26 10:36:26 +0530

    thumbnail component created

---
+++ components/MovieList.js
@@ -0,0 +1,22 @@
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+
+const MovieList = ({movies}) => {
+  return (
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3
+    3xl:flex flex-wrap justify-center">
+      {
+        movies.map((movie, index) => {
+          return (
+            <Thumbnail
+              key={index}
+              movie={movie}
+            />
+          )
+        })
+      }
+    </FlipMove>
+  )
+}
+
+export default MovieList;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import MovieList from '../components/MovieList'
 import Navbar from '../components/Navbar'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({results}) {
   return (
     <div>
       <Head>
@@ -15,6 +17,21 @@ export default function Home() {
       
       <Navbar />
       
+      <MovieList movies={results} /> 
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url ||  requests.fetchTrending.url}`).then((res) => {
+    return res.json();
+  })
+
+  return  {
+    props: {
+      results: request.results,
+    }
+  }
 }
